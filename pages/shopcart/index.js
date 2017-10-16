259d474b969925b2c2223d7a634a5125950dc3b8
luzuoquan <luzuoquanflex@gmail.com>
2017-10-16 22:54:29 +0800

    update shopcart

--- pages/shopcart/index.js
+++ pages/shopcart/index.js
@@ -3,7 +3,11 @@ const config = require('../../config/index.js')
 const baseUrl = config.developConfig.host
 Page({
   data: {
-    shopcart: []
+    shopcart: [],
+    checkboxItems: [
+      {name: 'standard is dealt for u.', value: '0', checked: true},
+      {name: 'standard is dealicient for u.', value: '1'}
+    ]
   },
   onShow() {
     const uuid = wx.getStorageSync('uuid')
@@ -18,5 +22,24 @@ Page({
         }
       }
     })
+  },
+  checkboxChange(e) {
+    console.log('checkbox发生change事件，携带value值为：', e.detail.value)
+
+    const checkboxItems = this.data.checkboxItems, values = e.detail.value
+    for (let i = 0, lenI = checkboxItems.length; i < lenI; ++i) {
+      checkboxItems[i].checked = false
+
+      for (let j = 0, lenJ = values.length; j < lenJ; ++j) {
+        if (checkboxItems[i].value == values[j]) {
+          checkboxItems[i].checked = true
+          break
+        }
+      }
+    }
+
+    this.setData({
+      checkboxItems: checkboxItems
+    });
   }
 })
